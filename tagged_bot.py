f5560d22ca23afc64ba4456c646535a1e18e8fc9
salimsalim1000 <66980168+salimsalim1000@users.noreply.github.com>
2025-10-07 02:40:38 +0200

    Update tagged_bot.py

--- tagged_bot.py
+++ tagged_bot.py
@@ -69,8 +69,9 @@ for _ in range(3):
                     EC.element_to_be_clickable(
                         (By.XPATH, "/html/body/div[1]/div/div[2]/div/div/div/div[1]/div[3]/div/div/form/div/textarea"))
                 )
-               # textarea.send_keys("hi honey .... dominant or submissive ?? ")
+              
                 textarea.send_keys("i want suc k u hard....")
+              # textarea.send_keys("hi honey .... dominant or submissive ?? ")
                 # Click send
                 sendit = WebDriverWait(driver, 10).until(
                     EC.element_to_be_clickable(
